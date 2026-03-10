3923992d092c8b60f0e3a2e697f4a92388666ed8
Anderson Oliveira <andersonlsoliveira02@gmail.com>
2026-03-10 00:40:11 -0300

    README

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,64 +9,95 @@ public class Main {
         int execucoes = 100;
 
         long[] temposInsercaoCrescente = new long[execucoes];
+        long[] temposInsercaoCrescenteDec = new long[execucoes];
         long[] temposInsercaoDecrescente = new long[execucoes];
+        long[] temposInsercaoDecrescenteDec = new long[execucoes];
         long[] temposInsercaoAleatorio = new long[execucoes];
+        long[] temposInsercaoAleatorioDec = new long[execucoes];
 
         long[] temposExclusaoCrescente = new long[execucoes];
+        long[] temposExclusaoCrescenteDec = new long[execucoes];
         long[] temposExclusaoDecrescente = new long[execucoes];
+        long[] temposExclusaoDecrescenteDec = new long[execucoes];
         long[] temposExclusaoAleatorio = new long[execucoes];
+        long[] temposExclusaoAleatorioDec = new long[execucoes];
 
         for(int i = 0; i < execucoes; i++){
 
             temposInsercaoCrescente[i] = inserirCrescente(n);
+            temposInsercaoCrescenteDec[i] = inserirCrescente(n);
             temposInsercaoDecrescente[i] = inserirDescrescente(n);
+            temposInsercaoDecrescenteDec[i] = inserirDescrescente(n);
             temposInsercaoAleatorio[i] = inserirAleatorio(n);
+            temposInsercaoAleatorioDec[i] = inserirAleatorio(n);
 
             temposExclusaoCrescente[i] = excluirCrescente(n);
+            temposExclusaoCrescenteDec[i] = excluirCrescente(n);
             temposExclusaoDecrescente[i] = excluirDecrescente(n);
+            temposExclusaoDecrescenteDec[i] = excluirDecrescente(n);
             temposExclusaoAleatorio[i] = excluirAleatorio(n);
+            temposExclusaoAleatorioDec[i] = excluirAleatorio(n);
         }
 
         double mediaInsercaoCrescente = media(temposInsercaoCrescente);
         double mediaInsercaoDecrescente = media(temposInsercaoDecrescente);
         double mediaInsercaoAleatorio = media(temposInsercaoAleatorio);
 
+        double mediaInsercaoCrescenteDec = media(temposInsercaoCrescenteDec);
+        double mediaInsercaoDecrescenteDec = media(temposInsercaoDecrescenteDec);
+        double mediaInsercaoAleatorioDec = media(temposInsercaoAleatorioDec);
+
         double desvioInsercaoCrescente = desvioPadrao(temposInsercaoCrescente, mediaInsercaoCrescente);
         double desvioInsercaoDecrescente = desvioPadrao(temposInsercaoDecrescente, mediaInsercaoDecrescente);
         double desvioInsercaoAleatorio = desvioPadrao(temposInsercaoAleatorio, mediaInsercaoAleatorio);
+
+        double desvioInsercaoCrescenteDec = desvioPadrao(temposInsercaoCrescenteDec, mediaInsercaoCrescenteDec);
+        double desvioInsercaoDecrescenteDec = desvioPadrao(temposInsercaoDecrescenteDec, mediaInsercaoDecrescenteDec);
+        double desvioInsercaoAleatorioDec = desvioPadrao(temposInsercaoAleatorioDec, mediaInsercaoAleatorioDec);
 
         double mediaExclusaoCrescente = media(temposExclusaoCrescente);
         double mediaExclusaoDecrescente = media(temposExclusaoDecrescente);
         double mediaExclusaoAleatorio = media(temposExclusaoAleatorio);
 
+        double mediaExclusaoCrescenteDec = media(temposExclusaoCrescenteDec);
+        double mediaExclusaoDecrescenteDec = media(temposExclusaoDecrescenteDec);
+        double mediaExclusaoAleatorioDec = media(temposExclusaoAleatorioDec);
+
         double desvioExclusaoCrescente = desvioPadrao(temposExclusaoCrescente, mediaExclusaoCrescente);
         double desvioExclusaoDecrescente = desvioPadrao(temposExclusaoDecrescente, mediaExclusaoDecrescente);
         double desvioExclusaoAleatorio = desvioPadrao(temposExclusaoAleatorio, mediaExclusaoAleatorio);
+
+        double desvioExclusaoCrescenteDec = desvioPadrao(temposExclusaoCrescenteDec, mediaExclusaoCrescenteDec);
+        double desvioExclusaoDecrescenteDec = desvioPadrao(temposExclusaoDecrescenteDec, mediaExclusaoDecrescenteDec);
+        double desvioExclusaoAleatorioDec = desvioPadrao(temposExclusaoAleatorioDec, mediaExclusaoAleatorioDec);
 
         System.out.println("Inserção\t\tOrdenação crescente\t\tOrdenação decrescente");
 
         System.out.println("Inserção crescente\t"
                 + mediaInsercaoCrescente + " +/- " + desvioInsercaoCrescente + "\t\t"
-                + mediaInsercaoCrescente + " +/- " + desvioInsercaoCrescente);
+                + mediaInsercaoCrescenteDec + " +/- " + desvioInsercaoCrescenteDec);
 
         System.out.println("Inserção decrescente\t"
                 + mediaInsercaoDecrescente + " +/- " + desvioInsercaoDecrescente + "\t\t"
-                + mediaInsercaoDecrescente + " +/- " + desvioInsercaoDecrescente);
+                + mediaInsercaoDecrescenteDec + " +/- " + desvioInsercaoDecrescenteDec);
 
         System.out.println("Inserção aleatória\t"
                 + mediaInsercaoAleatorio + " +/- " + desvioInsercaoAleatorio + "\t\t"
-                + mediaInsercaoAleatorio + " +/- " + desvioInsercaoAleatorio);
+                + mediaInsercaoAleatorioDec + " +/- " + desvioInsercaoAleatorioDec);
 
         System.out.println();
-        System.out.println("Exclusão:");
+        System.out.println("Exclusão\t\tOrdenação crescente\t\tOrdenação decrescente");
 
         System.out.println("Exclusão crescente\t"
-                + mediaExclusaoCrescente + " +/- " + desvioExclusaoCrescente);
+                + mediaExclusaoCrescente + " +/- " + desvioExclusaoCrescente + "\t\t"
+                + mediaExclusaoCrescenteDec + " +/- " + desvioExclusaoCrescenteDec);
 
         System.out.println("Exclusão decrescente\t"
-                + mediaExclusaoDecrescente + " +/- " + desvioExclusaoDecrescente);
+                + mediaExclusaoDecrescente + " +/- " + desvioExclusaoDecrescente + "\t\t"
+                + mediaExclusaoDecrescenteDec + " +/- " + desvioExclusaoDecrescenteDec);
 
         System.out.println("Exclusão aleatória\t"
-                + mediaExclusaoAleatorio + " +/- " + desvioExclusaoAleatorio);
+                + mediaExclusaoAleatorio + " +/- " + desvioExclusaoAleatorio + "\t\t"
+                + mediaExclusaoAleatorioDec + " +/- " + desvioExclusaoAleatorioDec);
     }
 }
